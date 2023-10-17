8491886a65fc6c6a53abf0d1d0fbd20719c84ac7
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-17 01:55:53 -0300

    feat: add new methods and pages

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -4,15 +4,75 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Support;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 
 class SupportController extends Controller
 {
-    public function index(Support $support)
+    public function index(Support $support): View
     {
         $supports = $support->all();
 
         return view('admin.supports.index', [
             'supports' => $supports,
         ]);
+    }
+
+    public function create(): View
+    {
+        return view('admin.supports.create');
+    }
+
+    public function store(Request $request, Support $support): RedirectResponse
+    {
+        $data = $request->only(['subject', 'body']);
+        $data['status'] = 'a';
+        $support->create($data);
+
+        return redirect()->route('supports.index');
+    }
+
+    public function show(Support $support, string|int $id): RedirectResponse|View
+    {
+        if (! $support = $support->find($id)) {
+            return back();
+        }
+
+        return view('admin.supports.show', [
+            'support' => $support,
+        ]);
+    }
+
+    public function edit(Support $support, string|int $id): View
+    {
+        if (! $support = $support->find($id)) {
+            return back();
+        }
+
+        return view('admin.supports.edit', [
+            'support' => $support,
+        ]);
+    }
+
+    public function update(Request $request, Support $support, string|int $id): RedirectResponse
+    {
+        if (! $support = $support->find($id)) {
+            return back();
+        }
+        $data = $request->only(['subject', 'body']);
+        $support->update($data);
+
+        return redirect()->route('supports.index');
+    }
+
+    public function destroy(Support $support, string|int $id): RedirectResponse
+    {
+        if (! $support = $support->find($id)) {
+            return back();
+        }
+        $support->delete();
+
+        return redirect()->route('supports.index');
     }
 }
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,19 +1,25 @@
 <h1>Listagem dos Suportes</h1>
 
+<a href="{{ route('supports.create') }}">Criar Dúvida</a>
+
 <table border="1">
     <thead>
         <th>Assunto</th>
         <th>Status</th>
         <th>Descrição</th>
-        <th></th>
-    </thead>
+        <thead>
+            </th>
+        </thead>
     <tbody>
         @foreach ($supports as $support)
             <tr>
                 <td>{{ $support->subject }}</td>
                 <td>{{ $support->status }}</td>
                 <td>{{ $support->body }}</td>
-                <td> >>Ações<< </td>
+                <td>
+                    <a href="{{ route('supports.show', $support->id) }}">Detalhes</a>
+                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                </td>
             </tr>
         @endforeach
     </tbody>
