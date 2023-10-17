56b81cc6d28726c8d7f3bcad1e396a7421b649fb
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-17 16:46:15 -0300

    feat: add SupportStatusEnum

---
+++ app/Enums/SupportStatusEnum.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+enum SupportStatusEnum: string
+{
+    case A = 'Open';
+    case C = 'Closed';
+    case P = 'Pending';
+
+    public static function fromValue(string $name): string
+    {
+        foreach (self::cases() as $status) {
+            if ($name === $status->name) {
+                return $status->value;
+            }
+        }
+
+        throw new \ValueError("$name is not a valid");
+    }
+}
