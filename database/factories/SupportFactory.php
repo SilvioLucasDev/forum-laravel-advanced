b0fd5b85dc5d1191f33f4385f4bd5346bbf81bc2
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-18 01:06:09 -0300

    refactor: change of id to uuid

--- database/factories/SupportFactory.php
+++ database/factories/SupportFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Enums\SupportStatusEnum;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,8 +18,10 @@ class SupportFactory extends Factory
      */
     public function definition(): array
     {
+
         return [
             'subject' => 'Por que o '.fake()->text(10).'?',
+            'user_id' => User::all()->random()->id,
             'status' => fake()->randomElement(SupportStatusEnum::cases()),
             'body' => 'Descrição da dúvida '.fake()->text(30),
         ];
