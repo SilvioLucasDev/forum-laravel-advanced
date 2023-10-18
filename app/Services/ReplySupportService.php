bca768153fd6104180b7ac7340e2c46f3bd2cabc
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-18 02:24:31 -0300

    feat: add ReplySupportService

---
+++ app/Services/ReplySupportService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Dtos\Replies\CreateReplyDTO;
+use App\Repositories\Contracts\ReplySupportRepositoryInterface;
+use stdClass;
+
+class ReplySupportService
+{
+    public function __construct(
+        protected ReplySupportRepositoryInterface $repository
+    ) {
+    }
+
+    public function getAllBySupportId(string $id): array
+    {
+        return $this->repository->getAllBySupportId($id);
+    }
+
+    public function create(CreateReplyDTO $dto): stdClass
+    {
+        return $this->repository->save($dto);
+    }
+}
