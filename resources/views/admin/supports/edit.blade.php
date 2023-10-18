6845812b0580bc7c847356b053326c23ed43a7c8
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-17 21:55:45 -0300

    feat: apply layout app and components

--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,8 +1,14 @@
-<h1>Editar Dúvida {{ $support->id }}</h1>
+@extends('admin.layouts.app')
 
-<x-alert />
+@section('title', 'Editar Dúvida')
 
-<form action="{{ route('supports.update', $support->id) }}" method="post">
-    @method('PUT')
-    @include('admin.supports.partials.form', ['support' => $support])
-</form>
+@section('header')
+    <h1 class="text-lg text-black-500">Editar Dúvida</h1>
+@endsection
+
+@section('content')
+    <form action="{{ route('supports.update', $support->id) }}" method="post">
+        @method('PUT')
+        @include('admin.supports.partials.form', ['support' => $support])
+    </form>
+@endsection
--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,28 +1,13 @@
-<h1>Listagem dos Suportes</h1>
+@extends('admin.layouts.app')
 
-<a href="{{ route('supports.create') }}">Criar Dúvida</a>
+@section('title', 'Fórum')
 
-<table border="1">
-    <thead>
-        <th>Assunto</th>
-        <th>Status</th>
-        <th>Descrição</th>
-        <thead>
-            </th>
-        </thead>
-    <tbody>
-        @foreach ($supports->items() as $support)
-            <tr>
-                <td>{{ $support->subject }} </td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">Detalhes</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+@section('header')
+    @include('admin.supports.partials.header')
+@endsection
 
-<x-pagination :paginator="$supports" :appends="$filters" />
+@section('content')
+    @include('admin.supports.partials.content')
+
+    <x-pagination :paginator="$supports" :appends="$filters" />
+@endsection
