37d8b3c746d7141cf5c43c6a240bb9cdbcc27f7f
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-18 02:24:54 -0300

    feat: apply new class in ReplySupportController

--- app/Http/Controllers/Admin/ReplySupportController.php
+++ app/Http/Controllers/Admin/ReplySupportController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\ReplySupportService;
 use App\Services\SupportService;
 use Illuminate\Contracts\View\View;
 
 class ReplySupportController extends Controller
 {
     public function __construct(
-        protected SupportService $supportService
+        protected SupportService $supportService,
+        protected ReplySupportService $replySupportService
     ) {
     }
 
@@ -18,9 +20,13 @@ class ReplySupportController extends Controller
         if (! $support = $this->supportService->findOne($id)) {
             return back();
         }
+        $replies = $this->replySupportService->getAllBySupportId($id);
+
+        dd($replies);
 
         return view('admin.supports.replies.index', [
             'support' => $support,
+            'replies' => $replies,
         ]);
     }
 }
