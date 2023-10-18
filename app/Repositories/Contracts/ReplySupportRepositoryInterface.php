d4a4e340683f6eb4fb1ac020c559d44ff1db613d
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-10-18 02:23:29 -0300

    feat: add ReplySupportRepositoryInterface

---
+++ app/Repositories/Contracts/ReplySupportRepositoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Dtos\Replies\CreateReplyDTO;
+use stdClass;
+
+interface ReplySupportRepositoryInterface
+{
+    public function getAllBySupportId(string $id): array;
+
+    public function save(CreateReplyDTO $dto): stdClass;
+}
